d5da8ae9c6da82bb8d818c1b34609c73f2e954f6
kingsley.ezenwere <kingsley.ezenwere@longbridgetech.com>
2025-08-01 01:17:06 +0100

    fix failing tests

--- test/e2e/app/domain/project/test_e2e_controller.py
+++ test/e2e/app/domain/project/test_e2e_controller.py
@@ -15,14 +15,14 @@ from main.app.domain.project.staging.service import ProjectStagingService
 from appodus_utils.db.session import close_db_engine
 
 
-from appodus import fast_api_app
+from appodus import app
 from appodus_utils.test.appodus_test_utils import TestUtils
 from starlette import status
 
 class TestProjectController(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
 
-        self.client = TestUtils.get_app_test_client(app=fast_api_app)
+        self.client = TestUtils.get_app_test_client(app=app)
         self.endpoint = "/v1/projects"
 
         self.project_dto = CreateProjectDto(
